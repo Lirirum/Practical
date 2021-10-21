b5498c27c0f7abed9553f956cae00b6a25f49da4
Lirirum <prime102102@gmail.com>
2021-10-21 16:55:29 +0300

    First

---
+++ ConsoleApplication8.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <iomanip>
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+using namespace std;
+int main()
+{
+    int min, max;
+    float x, h, y;
+
+    cout << "Enter min =";
+    cin >> min;
+    cout << "Enter max =";
+    cin >> max;
+    cout << "Enter h =";
+    cin >> h;
+
+
+    x = min;
+    cout << "\t x" << "\ty(x)" << endl;
+    while (x <= max + h / 2)
+    {
+        y = tan(x * M_PI / 180) + sqrt(x) + 2;
+        cout << setw(10) << x << setw(10) << setprecision(3) << y << endl;
+        x += h;
+    }
+
+
+
+    cout << "Do....While" << endl;
+    x = min;
+    cout << "\t x" << "\ty(x)" << endl;
+    do
+    {
+        y = tan(x * M_PI / 180) + sqrt(x) + 2;
+        cout << setw(10) << x << setw(10) << setprecision(3) << y << endl;
+        x += h;
+    } while (x <= max + h / 2);
+
+}
